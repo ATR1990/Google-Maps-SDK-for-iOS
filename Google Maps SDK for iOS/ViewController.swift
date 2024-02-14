f2b8595e120270a0db47acf988304b0ab2346ecf
AltynbekovT <tauyekelaltynbekov@gmail.com>
2024-02-14 18:24:20 +0600

    Initial Commit

---
+++ Google Maps SDK for iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google Maps SDK for iOS
+//
+//  Created by Тауекел on 14.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
